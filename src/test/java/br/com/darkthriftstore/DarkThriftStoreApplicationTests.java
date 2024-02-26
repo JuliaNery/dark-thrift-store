97ab3cac614463f826c2e7406578bdfbca1fd760
JuliaNery <julianerysinigalia@gmail.com>
2024-02-26 20:59:40 -0300

    Inicializando projeto

---
+++ src/test/java/br/com/darkthriftstore/DarkThriftStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.darkthriftstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkThriftStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

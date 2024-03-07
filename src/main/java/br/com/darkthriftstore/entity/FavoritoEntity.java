6eaf2594cf2702564ed61fc564c7e1bc830a1d07
Logon Aluno <logonrmlocal@fiap.com.br>
2024-03-06 22:27:32 -0300

    update entitys

--- src/main/java/br/com/darkthriftstore/entity/FavoritoEntity.java
+++ src/main/java/br/com/darkthriftstore/entity/FavoritoEntity.java
@@ -1,4 +1,18 @@
 package br.com.darkthriftstore.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
 public class FavoritoEntity {
+    @Id
+    private Long id;
+    private UsuarioEntity usuario;
+    private ProdutoEntity produto;
 }
